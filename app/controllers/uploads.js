ebcf89f07eec39fd80c079325b5f50f5f20506dd
Assisneto <netodamasceno12@gmail.com>
2018-06-22 21:52:33 -0300

    streaming de dados

--- app/controllers/uploads.js
+++ app/controllers/uploads.js
@@ -7,7 +7,7 @@ module.exports = (app)=>{
         let filename = req.headers.filename;
         console.log(`Arquivo Recebido: ${filename}`);
 
-        req.pipe(fs.createWriteStream(`app/files/${filename}`))
+        req.pipe(fs.createWriteStream(`app/files/${filename}.png`))
         .on('finish', ()=>{
             console.log("arquivo escrito");
             res.status(201).send('ok');
